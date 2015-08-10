d5cf6386f61216eef3135e70e132351ac8d42c8e
W Hayden Fuss <wifu1234@gmail.com>
2015-08-10 11:28:41 -0400

    Reorganized front end stuff to work with flask.

--- application.py
+++ application.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
